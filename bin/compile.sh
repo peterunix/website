c77a5aef551196b8a42b6386f153f72264f82663
p <p>
2022-04-11 18:43:22 +0100

    Compiled HTML

--- bin/compile.sh
+++ bin/compile.sh
@@ -12,7 +12,7 @@ function main {
   cat "$file" | pandoc --table-of-contents -s -c ../stylesheet/style.css -M title:Unixfandom -B header -A navbar -f org -t html -o "../content/html/$base.html"
   sed -e 's;</sub>;;g' \
 	  -e 's;<sub>;_;g' \
-	  -e 's;href=\"file:///;/;g' \
+	  -e 's;href=\"file:///;href\"/;g' \
 	  -e 's;<h1 class=\"title\">Unixfandom</h1>;<h1 class=\"title\"><a class=\"title\" href=/index.html>Unixfandom</h1>;g' "../content/html/$base.html" -i
 }
 
